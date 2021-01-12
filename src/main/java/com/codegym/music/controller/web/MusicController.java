62be384efdb343ee235684bb255b53aa7c2c97c9
HoangVietAnh <hoangvietanh1797@gmail.com>
2021-01-13 00:21:35 +0700

    Update musicInfo menu left

--- src/main/java/com/codegym/music/controller/web/MusicController.java
+++ src/main/java/com/codegym/music/controller/web/MusicController.java
@@ -1,13 +1,22 @@
 package com.codegym.music.controller.web;
 
+import com.codegym.music.model.Album;
+import com.codegym.music.model.Category;
+import com.codegym.music.model.Singer;
 import com.codegym.music.model.Song;
+import com.codegym.music.service.AlbumService;
+import com.codegym.music.service.CategoryService;
+import com.codegym.music.service.SingerService;
 import com.codegym.music.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,10 +27,36 @@ public class MusicController {
     @Autowired
     private SongService songService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private AlbumService albumService;
+
+    @Autowired
+    private SingerService singerService;
+
+    @ModelAttribute("categories")
+    public Iterable<Category> categories() {
+        return categoryService.findAll();
+    }
+
+    @ModelAttribute("albums")
+    public Iterable<Album> albums() {
+        return albumService.findAll();
+    }
+
+    @ModelAttribute("singers")
+    public Iterable<Singer> sings() {
+        return singerService.findAll();
+    }
+
+
     @GetMapping("{id}")
-    public String SongInfo(@PathVariable Long id, Pageable pageable, Model model){
+    public String SongInfo(@PathVariable Long id, Model model){
         Song song = songService.findById(id).get();
         Iterable<Song> concerning_song = songService.findAllBy5BySingerId(song.getSinger().getId(),id);
+        Pageable pageable = PageRequest.of(0,5, Sort.by(Sort.Direction.DESC, "views"));
         Page<Song> songs = songService.findAll(pageable);
         song.setViews(song.getViews()+1);
         songService.save(song);
