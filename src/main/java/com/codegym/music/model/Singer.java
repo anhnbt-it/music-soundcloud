6e61a53f10354e976183efd8405a8b0632d36c01
anhnbt-it <anhnbt.it@gmail.com>
2021-01-14 13:59:27 +0700

    Add field avatar & cover image

--- src/main/java/com/codegym/music/model/Singer.java
+++ src/main/java/com/codegym/music/model/Singer.java
@@ -1,5 +1,7 @@
 package com.codegym.music.model;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
@@ -9,12 +11,22 @@ public class Singer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     @NotEmpty
     private String name;
+
     @Column(nullable = false)
     @NotEmpty
     private String description;
+
+    private String avatarURL;
+    private String coverURL;
+
+    @Transient
+    private MultipartFile avatarFile;
+    @Transient
+    private MultipartFile coverFile;
 
     public Singer() {
     }
@@ -47,5 +59,37 @@ public class Singer {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getAvatarURL() {
+        return avatarURL;
+    }
+
+    public void setAvatarURL(String avatarURL) {
+        this.avatarURL = avatarURL;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
+    public MultipartFile getAvatarFile() {
+        return avatarFile;
+    }
+
+    public void setAvatarFile(MultipartFile avatarFile) {
+        this.avatarFile = avatarFile;
+    }
+
+    public MultipartFile getCoverFile() {
+        return coverFile;
+    }
+
+    public void setCoverFile(MultipartFile coverFile) {
+        this.coverFile = coverFile;
     }
 }
